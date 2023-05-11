243ae3ea554b13368abd3ade518c8d5862812d12
account75783 <54882293+account75783@users.noreply.github.com>
2023-05-11 02:01:18 -0700

    Create Call_of_Duty_forward.sh

---
+++ Call_of_Duty_forward.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Ask the user for the server and client IP
+read -p "Enter your server IP: " SERVER_IP  # Store the server IP provided by the user
+read -p "Enter your client IP: " CLIENT_IP  # Store the client IP provided by the user
+
+# Ask user if they want to add or delete the rules
+read -p "Do you want to add or delete the rules? (add/delete): " USER_ACTION  # Store the action provided by the user
+
+# Specify your WAN interface
+read -p "Enter your WAN interface (eg. eth0, enp1s0, etc): " INTERFACE # Define the network interface being used
+
+# Check if action is add or delete
+if [[ "$USER_ACTION" == "add" ]]; then  # If the user wants to add rules
+    ACTION='-A'  # Set ACTION to -A (Append)
+elif [[ "$USER_ACTION" == "delete" ]]; then  # If the user wants to delete rules
+    ACTION='-D'  # Set ACTION to -D (Delete)
+else
+    echo "Invalid action. Please enter 'add' or 'delete'."  # If an invalid action is provided, print an error and exit
+    exit 1
+fi
+
+# TCP Ports
+# Add or delete DNAT (Destination NAT) rule for the specified TCP ports
+iptables -t nat $ACTION PREROUTING -p tcp -i $INTERFACE -m multiport --dports 3074,27014:27050 -j DNAT --to-destination $CLIENT_IP
+
+# Add or delete FORWARD rule to allow specified TCP ports through firewall
+iptables $ACTION FORWARD -p tcp -d $CLIENT_IP -m multiport --dports 3074,27014:27050 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+
+# UDP Ports
+# Add or delete DNAT rule for the specified UDP ports
+iptables -t nat $ACTION PREROUTING -p udp -i $INTERFACE -m multiport --dports 3074,3478,4379:4380,27000:27031,27036 -j DNAT --to-destination $CLIENT_IP
+
+# Add or delete FORWARD rule to allow specified UDP ports through firewall
+iptables $ACTION FORWARD -p udp -d $CLIENT_IP -m multiport --dports 3074,3478,4379:4380,27000:27031,27036 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+
+# Drop known amplified UDP source ports (split into multiple rules due to 15 ports limit per rule)
+AMPLIFIED_PORTS1="19,53,123,161,37,111,137,389,445"  # Define first set of ports to be blocked
+AMPLIFIED_PORTS2="500,514,520,1900,2049,2086,2087"  # Define second set of ports to be blocked
+AMPLIFIED_PORTS3="3478,5060,11211"  # Define third set of ports to be blocked
+
+# Add or delete INPUT rules to drop packets from specified UDP source ports
+iptables $ACTION INPUT -p udp -d $CLIENT_IP -m multiport --sports $AMPLIFIED_PORTS1 -j DROP
+iptables $ACTION INPUT -p udp -d $CLIENT_IP -m multiport --sports $AMPLIFIED_PORTS2 -j DROP
+iptables $ACTION INPUT -p udp -d $CLIENT_IP -m multiport --sports $AMPLIFIED_PORTS3 -j DROP
